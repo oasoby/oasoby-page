696f8d17ffd6611b9a0f05e06218e4365fd6297b
oasoby <134684352+oasoby@users.noreply.github.com>
2023-10-22 15:04:31 +0900

    update

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-    <NuxtLoadingIndicator color="#10b981" :height="2" :duration="5000" :throttle="0"/>
+        <NuxtLoadingIndicator color="#10b981" :height="2" :duration="5000" :throttle="0"/>
     <NuxtLayout />
     <div class="content">
       <NuxtPage />
@@ -7,5 +7,4 @@
     <footer class="fixeld">
       <h2>©2023 oasobi</h2>
     </footer>
-    
 </template>
